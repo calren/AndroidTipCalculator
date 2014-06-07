331aa993b2ac2fd3370965309e6e3ee5093c9784
caren <cchang@tagged.com>
2014-06-07 12:38:19 -0700

    adding more listeners

--- app/src/main/java/com/codepath/example/tipcalculator/app/CalculateTipActivity.java
+++ app/src/main/java/com/codepath/example/tipcalculator/app/CalculateTipActivity.java
@@ -2,9 +2,12 @@ package com.codepath.example.tipcalculator.app;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +21,7 @@ public class CalculateTipActivity extends ActionBarActivity {
     SeekBar sbTipAmnt;
     TextView tvFinalAmnt;
     TextView tvTipPercent;
+    NumberPicker splitNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +32,58 @@ public class CalculateTipActivity extends ActionBarActivity {
         sbTipAmnt = (SeekBar) findViewById(R.id.sbTipAmnt);
         tvFinalAmnt = (TextView) findViewById(R.id.txtFinalAmnt);
         tvTipPercent = (TextView) findViewById(R.id.tvTipPercent);
-        tvFinalAmnt.setText(" ");
+        splitNumber = (NumberPicker) findViewById(R.id.numberPicker);
 
+        splitNumber.setMinValue(1);
+        splitNumber.setMaxValue(10);
+
+
+//        tvFinalAmnt.setText(" ");
+
+        setupSeekerListener();
+        setupNumberPickerListener();
+        setupBillAmountListener();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.calculate_tip, menu);
+        return true;
+    }
+
+    private void setupBillAmountListener() {
+        txtBillAmnt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                
+            }
+        });
+    }
+
+    private void setupNumberPickerListener() {
+        splitNumber.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                System.out.println(newVal);
+            }
+        });
+
+    }
+
+    private void setupSeekerListener() {
         sbTipAmnt.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChanged = 0;
             int billAmount = 0;
@@ -44,33 +98,13 @@ public class CalculateTipActivity extends ActionBarActivity {
             }
 
             public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
+
             }
 
             public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
         });
-//        lvItems = (ListView) findViewById(R.id.lvItems);
-//        preSorted = new TreeMap<String, Integer>();
-//        items = sort(preSorted);
-//        readItems();
-//        itemsAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, items);
-//        lvItems.setAdapter(itemsAdapter);
-//        items.add("Start adding items!");
-//        setupListViewListener();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.calculate_tip, menu);
-        return true;
-    }
-
-    private void setupSeekerListener() {
-
     }
 
     private void calculateTipAmount(int billAmnt) {
